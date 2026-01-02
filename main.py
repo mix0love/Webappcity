dc0a82d3785f9d4805056f1b06b90791669680fa
mix0love <vanyakurimov@gmail.com>
2026-01-03 00:56:29 +0100

    Remove web server startup from main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 import subprocess
 import threading
 
-# 1. Запускаем фейковый веб-порт для Render
 def run_web_server():
     port = int(os.environ.get("PORT", 10000))
     handler = http.server.SimpleHTTPRequestHandler
@@ -13,6 +12,6 @@ def run_web_server():
 
 threading.Thread(target=run_web_server, daemon=True).start()
 
-# 2. Запускаем твоего бота
+
 print("Запуск бота...")
 subprocess.call(["python", "bot.py"])
